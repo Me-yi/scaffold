ca0c91003f894a99403a186bcf6ca08523bf256a
AnthonytranDev <anthonytran.dev@gmail.com>
2025-02-21 17:21:48 +0000

    introduce basic playwright setup

---
+++ e2e/example.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+	await page.goto('/');
+
+	await expect(page).toHaveTitle(/Home/);
+});
+
+test('Deploy now link', async ({ page }) => {
+	await page.goto('/');
+
+	const firstLink = await page
+		.getByRole('link', { name: 'Deploy now' })
+		.nth(0)
+		.getAttribute('href');
+
+	expect(firstLink).toMatch(/vercel.com\//g);
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Image from 'next/image';
 
 export default function Home() {
@@ -7,6 +8,9 @@ export default function Home() {
 				'grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'
 			}
 		>
+			<Head>
+				<title>Home</title>
+			</Head>
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 				<Image
 					className="dark:invert"
